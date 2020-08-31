c8b41037c6ef5e567b141a9928cfb258a1182735
Victor <victorcwyu@gmail.com>
2020-08-31 00:49:03 -0400

    Added code to move focus to corresponding sections of page, based on clicking a menu item.  Also included a guard variable in case of the first site visit.

--- lesson2-focus/03-managing-focus/scripts/main.js
+++ lesson2-focus/03-managing-focus/scripts/main.js
@@ -1,26 +1,38 @@
-page('/', function() {
-  page.redirect('/what-is-vegemite');
+// Guard variable
+var isFirstPage = true;
+
+page("/", function () {
+  page.redirect("/what-is-vegemite");
 });
 
-page('/:slug', function(context) {
+page("/:slug", function (context) {
   // This will match any value after the first / in the url. For example, if
   // the url was /foo, the value of slug would be "foo".
   var slug = context.params.slug;
 
   // Remove is-active class from previous menu item and section
-  var oldMenuItem = document.querySelector('#menu .is-active');
-  var oldPage = document.querySelector('main .is-active');
-  oldMenuItem.classList.remove('is-active');
-  oldPage.classList.remove('is-active');
+  var oldMenuItem = document.querySelector("#menu .is-active");
+  var oldPage = document.querySelector("main .is-active");
+  oldMenuItem.classList.remove("is-active");
+  oldPage.classList.remove("is-active");
 
   // Add is-active class to new menu item and section using the URL slug
-  var newMenuItem = document.querySelector('#menu [data-page='+slug+']');
-  var newPage = document.querySelector('main [data-page='+slug+']');
-  newMenuItem.classList.add('is-active');
-  newPage.classList.add('is-active');
+  var newMenuItem = document.querySelector("#menu [data-page=" + slug + "]");
+  var newPage = document.querySelector("main [data-page=" + slug + "]");
+  newMenuItem.classList.add("is-active");
+  newPage.classList.add("is-active");
 
+  // If this is the first site visit, don't move focus
+  // Wait until a menu item is clicked
+  if (isFirstPage) {
+    isFirstPage = false;
+    return;
+  }
+
+  // Move the focus to the heading of the corresponding section of the page
+  newPage.querySelector("h2").focus();
 });
 
 page({
-  hashbang: true
+  hashbang: true,
 });
