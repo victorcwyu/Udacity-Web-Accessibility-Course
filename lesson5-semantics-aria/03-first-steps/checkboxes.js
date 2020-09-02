94d1306533234e3599dcccd49cd62ed8e1715c58
Victor <victorcwyu@gmail.com>
2020-09-02 16:00:09 -0400

    Added code to initialize the role and aria-checked state of the checkbox.

--- lesson5-semantics-aria/03-first-steps/checkboxes.js
+++ lesson5-semantics-aria/03-first-steps/checkboxes.js
@@ -1,13 +1,13 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
   /** Define values for keycodes */
-  var VK_ENTER      = 13;
-  var VK_SPACE      = 32;
-  var VK_LEFT       = 37;
-  var VK_UP         = 38;
-  var VK_RIGHT      = 39;
-  var VK_DOWN       = 40;
+  var VK_ENTER = 13;
+  var VK_SPACE = 32;
+  var VK_LEFT = 37;
+  var VK_UP = 38;
+  var VK_RIGHT = 39;
+  var VK_DOWN = 40;
 
   /** Helper function to convert NodeLists to Arrays */
   function slice(nodes) {
@@ -17,14 +17,20 @@
   function Checkbox(el) {
     this.el = el;
 
-    this.el.addEventListener('keydown', this.handleKeyDown.bind(this));
-    this.el.addEventListener('click', this.toggle.bind(this));
+    this.el.addEventListener("keydown", this.handleKeyDown.bind(this));
+    this.el.addEventListener("click", this.toggle.bind(this));
 
-    // Any other set-up we want to do here?
+    // Initialize the role and aria-checked state of the checkbox.
+    this.el.setAttribute("role", "checkbox");
+    if (this.el.hasAttribute("checked")) {
+      this.el.setAttribute("aria-checkbox", "true");
+    } else {
+      this.el.setAttribute("aria-checkbox", "false");
+    }
   }
 
-  Checkbox.prototype.handleKeyDown = function(e) {
-    switch(e.keyCode) {
+  Checkbox.prototype.handleKeyDown = function (e) {
+    switch (e.keyCode) {
       case VK_ENTER:
       case VK_SPACE: {
         this.toggle();
@@ -33,22 +39,18 @@
     }
   };
 
-  Checkbox.prototype.toggle = function() {
-    if (this.el.hasAttribute('checked')) {
-      this.el.removeAttribute('checked');
+  Checkbox.prototype.toggle = function () {
+    if (this.el.hasAttribute("checked")) {
+      this.el.removeAttribute("checked");
 
       // Hmm.
-
     } else {
-      this.el.setAttribute('checked', '');
+      this.el.setAttribute("checked", "");
 
       // Hmmmmm.
-
     }
   };
 
-  var checkboxes = slice(document.querySelectorAll('.checkbox'));
-  for (var checkbox of checkboxes)
-    checkbox.logic = new Checkbox(checkbox);
-
-}());
+  var checkboxes = slice(document.querySelectorAll(".checkbox"));
+  for (var checkbox of checkboxes) checkbox.logic = new Checkbox(checkbox);
+})();
