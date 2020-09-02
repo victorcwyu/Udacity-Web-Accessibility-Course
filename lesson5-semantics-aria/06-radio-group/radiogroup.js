a193304cbbff3dbb3785868194373b7f06b37c88
Victor <victorcwyu@gmail.com>
2020-09-02 16:36:40 -0400

    Added code to keep checked and aria-checked attributes in sync.

--- lesson5-semantics-aria/06-radio-group/radiogroup.js
+++ lesson5-semantics-aria/06-radio-group/radiogroup.js
@@ -87,14 +87,14 @@
     // Set the old button to tabindex -1
     this.focusedButton.tabIndex = -1;
     this.focusedButton.removeAttribute("checked");
+    this.focusedButton.setAttribute("aria-checked", "false");
 
     // Set the new button to tabindex 0 and focus it
     this.focusedButton = this.buttons[this.focusedIdx];
     this.focusedButton.tabIndex = 0;
     this.focusedButton.focus();
     this.focusedButton.setAttribute("checked", "");
-
-    // ... we probably want to do some stuff here, too ...
+    this.focusedButton.setAttribute("aria-checked", "true");
   };
 
   var group1 = new RadioGroup("#group1");
